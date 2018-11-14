abb8ca9bce91b3b6dcc542718dbe0211b6347071
Diosdavi Lara <diosdavi@megsoftconsulting.com>
2018-11-14 03:02:41 -0500

    Change implementation to use PriorityQueue

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import concurrent.futures
 import urllib.request
 import time
-import heapq
+import queue
 
-priority_queue = []
+priority_queue = queue.PriorityQueue()
 
 URLS = [(2, 'http://www.foxnews.com/'),
         (5, 'http://www.cnn.com/'),
         (7, 'http://europe.wsj.com/'),
         (1, 'http://www.bbc.co.uk/'),
-        (1, 'http://www.npr.org/'),
-        (9, 'http://www.facebook.com/'),
+        (9, 'http://www.npr.org/'),
+        (1, 'http://www.facebook.com/'),
         (8, 'http://www.google.com/'),
         (4, 'http://www.twitter.com/'),
         (2, 'http://www.youtube.com/'),
@@ -23,7 +23,7 @@ def task_generator(spacing):
     while True:
         for priority, url in URLS:
             time.sleep(spacing)
-            heapq.heappush(priority_queue, (priority, url))
+            priority_queue.put((priority, url))
             print("Added URL task with priority " + str(priority) + " to the queue")
 
 
@@ -37,7 +37,7 @@ def load_url(url, priority, timeout):
 with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
 
     # start a thread for the task_generator method, which sends work in through the queue
-    future_to_url = {executor.submit(task_generator, 1.0): 'FEEDER DONE'}
+    future_to_url = {executor.submit(task_generator, 0.5): 'FEEDER DONE'}
 
     while future_to_url:
         # check for status of the tasks which are currently working
@@ -46,13 +46,13 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
             return_when=concurrent.futures.FIRST_COMPLETED)
         print("Finished threads: " + str(len(done)) +
               ", unfinished threads: " + str(len(not_done)) +
-              ", queue size: " + str(len(priority_queue)))
+              ", queue size: " + str(priority_queue.qsize()))
 
         # if there is incoming work, start a new task(future)
-        while priority_queue:
+        while not priority_queue.empty():
 
             # fetch a url with its priority from the queue
-            priority, url = heapq.heappop(priority_queue)
+            priority, url = priority_queue.get()
 
             # Start the load operation and mark the task with its URL
             future_to_url[executor.submit(load_url, url, priority, 60)] = url
