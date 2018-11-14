f19759c9841504494c158f492d66dbc70a1d3bd5
Diosdavi Lara <diosdavi@megsoftconsulting.com>
2018-11-14 02:35:46 -0500

    Add comments for clarity

--- main.py
+++ main.py
@@ -18,12 +18,13 @@ URLS = [(2, 'http://www.foxnews.com/'),
         (3, 'http://www.reddit.com/')]
 
 
-def feed_the_workers(spacing):
-    """ Simulate outside actors sending in work to do """
+def task_generator(spacing):
+    """ Generates urls in real time to send to the queue """
     while True:
         for priority, url in URLS:
             time.sleep(spacing)
             heapq.heappush(priority_queue, (priority, url))
+            print("Added URL task with priority " + str(priority) + " to the queue")
 
 
 def load_url(url, priority, timeout):
@@ -33,14 +34,13 @@ def load_url(url, priority, timeout):
         return conn.read()
 
 
-# We can use a with statement to ensure threads are cleaned up promptly
 with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
 
-    # start a future for a thread which sends work in through the queue
-    future_to_url = {executor.submit(feed_the_workers, 1.0): 'FEEDER DONE'}
+    # start a thread for the task_generator method, which sends work in through the queue
+    future_to_url = {executor.submit(task_generator, 1.0): 'FEEDER DONE'}
 
     while future_to_url:
-        # check for status of the futures which are currently working
+        # check for status of the tasks which are currently working
         done, not_done = concurrent.futures.wait(
             future_to_url, timeout=5.0,
             return_when=concurrent.futures.FIRST_COMPLETED)
@@ -48,16 +48,16 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
               ", unfinished threads: " + str(len(not_done)) +
               ", queue size: " + str(len(priority_queue)))
 
-        # if there is incoming work, start a new future
+        # if there is incoming work, start a new task(future)
         while priority_queue:
 
             # fetch a url with its priority from the queue
             priority, url = heapq.heappop(priority_queue)
 
-            # Start the load operation and mark the future with its URL
+            # Start the load operation and mark the task with its URL
             future_to_url[executor.submit(load_url, url, priority, 60)] = url
 
-        # process any completed futures
+        # process any completed tasks
         for future in done:
             url = future_to_url[future]
             try:
@@ -65,10 +65,7 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
             except Exception as exc:
                 print('%r generated an exception: %s' % (url, exc))
             else:
-                if url == 'FEEDER DONE':
-                    print(data)
-                else:
-                    print('%r page is %d bytes' % (url, len(data)))
+                print('%r page is %d bytes' % (url, len(data)))
 
-            # remove the now completed future
+            # allow the thread from the now completed task to be used again
             del future_to_url[future]
